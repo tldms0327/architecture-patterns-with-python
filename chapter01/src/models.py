3b262c12b789fcb8f3af7ef319ce5bf71786bb19
Karin L <karin@daangn.com>
2023-08-16 02:22:00 +0900

    feat: pass test_batches

--- chapter01/src/models.py
+++ chapter01/src/models.py
@@ -1,10 +1,37 @@
+from dataclasses import dataclass
+from datetime import date
+from typing import List, Optional, Set
+@dataclass(frozen=True)
 class OrderLine:
-    pass
+    orderid: str
+    sku: str
+    qty: int
 
 
 class Batch:
-    pass
+    def __init__(self, ref:str, sku:str, qty: int, eta: Optional[date]):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self.available_quantity = qty
+        self.allocated_orders: Set[str] = set()
 
 
-def allocate():
-    pass
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line) and line.orderid not in self.allocated_orders:
+            self.available_quantity -= line.qty
+            self.allocated_orders.add(line.orderid)
+            
+    def deallocate(self, line: OrderLine):
+        if line.orderid in self.allocated_orders:
+            self.allocated_orders.remove(line.orderid)
+            self.available_quantity += line.qty
+                    
+    def can_allocate(self, line: OrderLine):
+        if self.available_quantity >= line.qty and self.sku == line.sku:
+            return True
+        return False
+
+     
+def allocate(line: OrderLine, batches: List[Batch]):
+       return True
